6af09b1b19a349f5777395127b4db1472157aa34
ssstefannn <steffpotter@gmail.com>
2022-06-04 16:58:14 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mathemat.io.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

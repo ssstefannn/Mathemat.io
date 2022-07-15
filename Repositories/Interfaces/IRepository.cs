7353ce27ff70758ec2e553bff39c7a93102301b8
ssstefannn <steffpotter@gmail.com>
2022-07-15 21:23:07 +0200

    implement irepository

---
+++ Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace Mathematio.Repositories.Interfaces
+{
+    public interface IRepository<T>
+    {
+        public ICollection<T> GetAll(object searchModel);
+
+        public ICollection<T> GetOne(object searchId);
+
+        public T Add(T model);
+
+        public T Update(object newModel);
+
+        public object Delete(object searchId);
+
+
+    }
+}
---
+++ Repositories/Repository.cs
@@ -0,0 +1,46 @@
+﻿using Mathemat.io.Data;
+using Mathematio.Repositories.Interfaces;
+
+namespace Mathematio.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+
+        private readonly MathematioContext _context;
+
+        public Repository(MathematioContext context)
+        {
+            _context = context;
+        }
+
+        public T Add(T model)
+        {
+            _context.Add(model);
+            _context.SaveChanges();
+            return model;
+           
+        }
+
+        public object Delete(object searchId)
+        {
+            _context.Remove(searchId);
+            _context.SaveChanges();
+            return true;
+        }
+
+        public ICollection<T> GetAll(object searchModel)
+        {
+            //TODO : use reflection on _context.entity
+        }
+
+        public ICollection<T> GetOne(object searchId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Update(object newModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
